c74b684c222871dcd1492200ab6464afa856686c
wHo69 <85286288+wHo69@users.noreply.github.com>
2021-08-17 11:27:52 +0800

    Create client.py
    Incomplete, bugged

---
+++ client.py
@@ -0,0 +1,42 @@
+#Client script for chat
+#socket ip -> input
+#socket port = 6942
+
+import socket
+import sys
+import threading
+import atexit
+
+ip = input("IP pls: ")
+port = 6942
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created")
+except socket.error as e:
+    print("Error while creating socket.\nDebug log:", e)
+    sys.exit()
+
+s.connect((ip, port))
+print("Connected to %s:%s" %(ip, port))
+username = input("\nUsername: ")
+print("\nType a message and press enter to send :)\n")
+
+def sendMsg():
+    while True:
+        msg = input("")
+        while msg == "":
+            msg = input("")
+        s.send((username + ": " + msg).encode())
+
+def exit_handler():
+    s.close()
+
+
+msgThread = threading.Thread(target=sendMsg)
+msgThread.start()
+atexit.register(exit_handler)
+while True:
+    rmsg = s.recv(1024)
+    if not rmsg:
+        print(rmsg.decode())
